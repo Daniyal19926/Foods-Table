d7ef08b496c64dc7d0975053ef9b7856c1535983
Daniyal <you@example.com>
2024-01-18 19:41:23 +0100

    reusable text coulmns

--- src/components/FoodsTable.tsx
+++ src/components/FoodsTable.tsx
@@ -27,7 +27,12 @@ function FoodsTable({ foods, sortColumn, onSort, onDelete, onFavour }: Props) {
   return (
     <table className="table">
       <TableHeader onSort={onSort} sortColumn={sortColumn} columns={columns} />
-      <TableBody foods={foods} onDelete={onDelete} onFavour={onFavour} />
+      <TableBody
+        columns={columns}
+        foods={foods}
+        onDelete={onDelete}
+        onFavour={onFavour}
+      />
     </table>
   );
 }
--- src/components/TableBody.tsx
+++ src/components/TableBody.tsx
@@ -1,21 +1,27 @@
+import _ from "lodash";
 import { Food } from "../services/fakeFoodService";
 import Favourite from "./Favourite";
+import { Column } from "./TableHeader";
 
 interface Props {
   foods: Food[];
+  columns: Column[];
   onDelete(id: string): void;
   onFavour(id: string): void;
 }
 
-function TableBody({ foods, onDelete, onFavour }: Props) {
+function TableBody({ foods, columns, onDelete, onFavour }: Props) {
   return (
     <tbody>
       {foods.map((food) => (
         <tr key={food._id}>
-          <td>{food.name}</td>
-          <td>{food.category.name}</td>
-          <td>{food.price}</td>
-          <td>{food.numberInStock}</td>
+          {columns.map(
+            (column) =>
+              "path" in column && (
+                <td key={column.path}>{_.get(food, column.path)}</td>
+              )
+          )}
+
           <td>
             <Favourite
               isFavoured={Boolean(food.isFavoured)}
