ed75bd069fefae03a9ae47555085c3a9bd9ec161
Daniyal <you@example.com>
2024-01-30 00:23:18 +0100

    added not found page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as FoodsPage } from "./FoodsPage";
 
 export { default as CustomersPage } from "./CustomersPage";
 export { default as OrdersPage } from "./OrdersPage";
+export { default as NotFoundePage } from "./NotfoundPage";
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import App from "@App";
 import { CustomersPage, FoodsPage, OrdersPage } from "@pages";
+import NotFoundPage from "@pages/NotfoundPage";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <NotFoundPage />,
     children: [
       { path: "/foods", element: <FoodsPage /> },
       {
