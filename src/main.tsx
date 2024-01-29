0ba5eba152d2707b154e80f1e0dd9d53ea06d14c
Daniyal <you@example.com>
2024-01-29 23:21:37 +0100

    added routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "@App.tsx";
+import { RouterProvider } from "react-router-dom";
+import router from "@routes";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/routes.tsx
@@ -0,0 +1,19 @@
+import App from "@App";
+import { CustomersPage, OrdersPage } from "@pages";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/customers",
+    element: <CustomersPage />,
+  },
+  {
+    path: "/orders",
+    element: <OrdersPage />,
+  },
+]);
+export default router;
