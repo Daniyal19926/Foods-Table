7014de0bf105258bb43c4e5848232e61ee9b9646
Daniyal <you@example.com>
2024-01-30 01:20:59 +0100

    linked food items with food form

--- src/components/FoodsTable.tsx
+++ src/components/FoodsTable.tsx
@@ -1,5 +1,6 @@
 import { Column, Food, SortColumn } from "@types";
 import { Favourite, Table } from "@components/common";
+import { Link } from "react-router-dom";
 
 interface Props {
   foods: Food[];
@@ -11,7 +12,13 @@ interface Props {
 
 function FoodsTable({ foods, sortColumn, onSort, onDelete, onFavour }: Props) {
   const columns: Column<Food>[] = [
-    { path: "name", label: "Name" },
+    {
+      path: "name",
+      label: "Name",
+
+      content: (food) => <Link to={`/foods/${food._id}`}>{food.name}</Link>,
+    },
+
     { path: "category.name", label: "Category" },
     { path: "price", label: "Price" },
     { path: "numberInStock", label: "Stock" },
