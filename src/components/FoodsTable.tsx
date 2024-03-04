62e1cebb56bfdd9e6f39d829db6284651201eb0b
Daniyal <you@example.com>
2024-03-04 18:51:49 +0100

    hid delete button for not logged users

--- src/components/FoodsTable.tsx
+++ src/components/FoodsTable.tsx
@@ -1,6 +1,7 @@
 import { Column, Food, SortColumn } from "@types";
 import { Favourite, Table } from "@components/common";
 import { Link } from "react-router-dom";
+import { auth } from "@services";
 
 interface Props {
   foods: Food[];
@@ -11,6 +12,7 @@ interface Props {
 }
 
 function FoodsTable({ foods, sortColumn, onSort, onDelete, onFavour }: Props) {
+  const user = auth.getCurrentUser();
   const columns: Column<Food>[] = [
     {
       key: "name",
@@ -35,9 +37,16 @@ function FoodsTable({ foods, sortColumn, onSort, onDelete, onFavour }: Props) {
     {
       key: "delete",
       content: (food) => (
-        <button className="btn btn-danger" onClick={() => onDelete(food.id)}>
-          Delete
-        </button>
+        <>
+          {user?.isAdmin && (
+            <button
+              className="btn btn-danger"
+              onClick={() => onDelete(food.id)}
+            >
+              Delete
+            </button>
+          )}
+        </>
       ),
     },
   ];
